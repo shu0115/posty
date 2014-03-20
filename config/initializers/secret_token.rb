715f5a3291d8cd5e5b8041cc129c8a59046090bc
shu0115 <s.matsumoto0115@gmail.com>
2014-03-20 15:12:47 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Posty::Application.config.secret_key_base = '574dc64abca8e366cf4b7147dc2dd17a006a7c0666f190d2eebb1c70ec0916448e8a41dc543e4f54b78a464e77c8e24e29bf3e121266adf16857b319e5ac24e3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Posty::Application.config.session_store :cookie_store, key: '_posty_session'
