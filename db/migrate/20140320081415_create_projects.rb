104eca1e48e9bfca51d94e68ffcf9935c13983c4
shu0115 <s.matsumoto0115@gmail.com>
2014-03-20 18:14:33 +0900

    マイグレーション修正

--- db/migrate/20140320081415_create_projects.rb
+++ db/migrate/20140320081415_create_projects.rb
@@ -1,10 +1,10 @@
 class CreateProjects < ActiveRecord::Migration
   def change
-    create_table :projects do |t|
-      t.references :user, index: true
-      t.string :name
+    # create_table :projects do |t|
+    #   t.references :user, index: true
+    #   t.string :name
 
-      t.timestamps
-    end
+    #   t.timestamps
+    # end
   end
 end
